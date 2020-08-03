6264dc45ef4aa096107c0158a65c7e74349c9f37
chaodoing <chaodoing@live.com>
2020-08-03 13:04:24 +0800

    修改controller

--- controller.go
+++ controller.go
@@ -28,7 +28,7 @@ func (this *Controller) Page(invoke func(page, limit, offset int) (data interfac
 	}
 	data, total, err := invoke(page, limit, offset)
 	if err != nil {
-		this.Fail(err.Error())
+		this.Json(1, "Fail", err.Error())
 	} else {
 		this.App.Response.Page(data, page, limit, total)
 	}
@@ -51,6 +51,7 @@ func (this *Controller) Destruct() {
 	this.App = nil
 }
 
+
 // @title 获取 *gorm.DB
 // @description 获取数据库操作对象
 // @param config *Config 配置类
@@ -72,61 +73,6 @@ func (this *Controller) Db() *gorm.DB {
 	return this.db
 }
 
-// @title 输出成功消息
-// @description 输出成功消息 (data, message, code)
-// @param data interface 成功数据
-// @param message string 成功消息
-// @param code    int    成功代码
-func (this *Controller) Success(data interface{}, opts ...interface{}) {
-	var (
-		Message string
-		Code    int
-	)
-	if len(opts) == 1 {
-		Message = opts[0].(string)
-		Code = 0
-	} else if len(opts) == 2 {
-		Message = opts[0].(string)
-		Code = opts[1].(int)
-	} else {
-		Message = "Ok"
-		Code = 0
-	}
+func (this *Controller) Json(Code int, Message string, data interface{}) {
 	this.App.Response.Json(Code, Message, data)
-}
-
-// @title 输出内容
-// @description 输出内容 (message, [code])
-// @param message string 输出的消息
-// @param code int 错误代码
-func (this *Controller) Message(message string, code ...int) {
-	var Code int
-	if len(code) > 0 {
-		Code = code[0]
-	} else {
-		Code = 0
-	}
-	this.App.Response.Json(Code, message, nil)
-}
-
-// @title 错误输出
-// @description 错误消息输出 (message, code, data)
-// @param message string 错误消息
-// @param code int 错误代码
-// @param data interface{}  错误数据
-func (this *Controller) Fail(message string, opts ...interface{}) {
-	var (
-		Data interface{}
-		Code int
-	)
-	if len(opts) == 1 {
-		Code = opts[0].(int)
-	} else if len(opts) == 2 {
-		Code = opts[0].(int)
-		Data = opts[1]
-	} else {
-		Code = 1
-		Data = nil
-	}
-	this.App.Response.Json(Code, message, Data)
 }
